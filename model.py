8f0d2a5213fc0a13b9fcabbc008f4bffe2be61a6
eligohar <eligohar@users.noreply.github.com>
2018-06-07 15:36:55 +0900

    fllow tis now

--- model.py
+++ model.py
@@ -14,3 +14,25 @@ from sklearn.utils import compute_class_weight
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import classification_report, roc_curve, auc, precision_recall_curve
 import os
+
+
+import numpy as np
+import pandas as pd
+from __future__ import print_function
+
+# To plot pretty figures
+%matplotlib inline 
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import Imputer
+#matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras_contrib.layers import SReLU
+from keras.callbacks import BaseLogger, EarlyStopping, ReduceLROnPlateau, TensorBoard
+from sklearn.preprocessing import StandardScaler
+from sklearn.utils import compute_class_weight
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report, roc_curve, auc, precision_recall_curve
+import os
